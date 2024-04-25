2c6a4bc4dc005f913ae1762c12086e009e1246f0
JianChenLSX <jian.chen@lascaux.it>
2024-04-25 22:38:29 +0200

    added stepper template and logic

--- src/app/stepper/stepper.component.ts
+++ src/app/stepper/stepper.component.ts
@@ -1,12 +1,90 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {NgClass, NgForOf} from "@angular/common";
+import {Step} from "../models/step.model";
+import {StepperService} from "../services/stepper.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-stepper',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgClass
+  ],
   templateUrl: './stepper.component.html',
   styleUrl: './stepper.component.scss'
 })
-export class StepperComponent {
+export class StepperComponent implements OnDestroy {
+  currentStep?: Step;
+  steps: Step[];
+
+  private subscriptions: Subscription[] = [];
+  constructor(private stepperService: StepperService) {
+    this.steps = [];
+    this.steps.push(
+      new Step({
+        route: 'choose-model',
+        description: 'Step 1',
+        index: 1,
+        id: 'step1',
+        disabled: true
+      }),
+      new Step({
+        route: 'choose-options',
+        description: 'Step 2',
+        index: 2,
+        id: 'step2',
+        disabled: true
+      }),
+      new Step({
+        route: 'summary',
+        description: 'Step 3',
+        index: 3,
+        id: 'step3',
+        disabled: true
+      })
+    );
+
+    this.subscriptions.push(
+      this.stepperService.getCurrentStepIdObservable().subscribe(
+        stepId => this.currentStep = this.steps.find(step => step.id === stepId)
+      )
+    );
+
+    this.subscriptions.push(
+      this.stepperService.getCurrentStepCompleted().subscribe(
+        // completion is computed inside each page after the navigation
+        completed => {
+          if (completed) {
+            this.steps.forEach(step => {
+              // enable the next step and the previously completed
+              if (this.currentStep && (step.index - 1) <= this.currentStep.index) {
+                step.disabled = false;
+              } else {
+                step.disabled = true;
+              }
+            })
+          } else {
+            this.steps.forEach(step => {
+              // enable the previously completed
+              if (this.currentStep && step.index <= this.currentStep.index) {
+                step.disabled = false;
+              } else {
+                step.disabled = true;
+              }
+            })
+          }
+        }
+      )
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
+
+  changeStep(selectedStep: Step) {
+    // TODO navigation
+  }
 
 }
