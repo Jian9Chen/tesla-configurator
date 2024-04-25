3e1bdd6eeb1e5e21061b2883d97f949f6c4d79a0
JianChenLSX <jian.chen@lascaux.it>
2024-04-25 22:35:33 +0200

    basic configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import {Component} from '@angular/core';
 import {AsyncPipe, JsonPipe} from '@angular/common';
+import {StepperComponent} from "./stepper/stepper.component";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe],
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-  `,
+  imports: [AsyncPipe, JsonPipe, StepperComponent, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  name = 'Angular';
-
+  title: string = 'tesla-configurator';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'choose-model',
+    pathMatch: 'full'
+  },
+  {
+    path: 'choose-model',
+    loadComponent: () => import('./pages/models/models.component').then(mod => mod.ModelsComponent)
+  },
+  {
+    path: 'choose-options',
+    loadComponent: () => import('./pages/options/options.component').then(mod => mod.OptionsComponent)
+    // TODO set can activate
+  },
+  {
+    path: 'summary',
+    loadComponent: () => import('./pages/price-summary/price-summary.component').then(mod => mod.PriceSummaryComponent)
+    // TODO set can activate
+  },
+  {
+    path: "**",
+    redirectTo: 'choose-model'
+  }
+];
