e1e642589dc08487973671f262bc9d714a152370
JianChenLSX <jian.chen@lascaux.it>
2024-04-26 12:16:38 +0200

    created car configuration service

--- src/app/services/car-configuration.service.ts
+++ src/app/services/car-configuration.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {CarConfiguration, SelectedCarConfiguration} from "../models/car.configuration.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarConfigurationService {
 
-  constructor() { }
+  private selectedConfigurationSubject: BehaviorSubject<SelectedCarConfiguration | null> = new BehaviorSubject<SelectedCarConfiguration | null>(null);
+  constructor(private httpClient: HttpClient) { }
+
+  getSelectedConfigurationValue() {
+    return this.selectedConfigurationSubject.value;
+  }
+
+  setSelectedConfiguration(configuration: SelectedCarConfiguration | null) {
+    this.selectedConfigurationSubject.next(configuration);
+  }
+  getAvailableConfigurations(modelCode: string) {
+    return this.httpClient.get<CarConfiguration>(`http://localhost:4200/options/${modelCode}`);
+  }
 }
